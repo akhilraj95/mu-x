b75f5df6e9e9aee4e5c9e429ea140e6174ced8b4
akhil.a <akhil@akhil.a>
2017-06-22 16:13:39 +0530

    spring init

---
+++ mux/src/main/java/com/endurance/india/mux/mux/MuxApplication.java
@@ -0,0 +1,12 @@
+package com.endurance.india.mux.mux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MuxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MuxApplication.class, args);
+	}
+}
---
+++ mux/src/test/java/com/endurance/india/mux/mux/MuxApplicationTests.java
@@ -0,0 +1,16 @@
+package com.endurance.india.mux.mux;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MuxApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
